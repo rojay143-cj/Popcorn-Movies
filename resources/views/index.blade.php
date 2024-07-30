2695b90b3751e767ba0666753ad876afb4f1140d
rojay143-cj <rojaymerlin@gmail.com>
2024-07-30 19:57:02 +0800

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
                         <li><a href="">Trending</a></li>
                         <li><a href="">Latest Release</a></li>
                     </ul>
-                    <section class="mt-5 mb-2 w-full flex">
+                    <section class="mb-2 w-full flex">
                         <div class="relative" style="width:600px">
                             <input type="text" placeholder="Search your movie..." id="search" class="w-full h-9 rounded-lg px-5 bg-gray-800 opacity-60 pl-10">
                             <button class="absolute inset-y-0 left-0 pl-3 flex items-center text-gray-400">
@@ -46,9 +46,9 @@
             <span class="w-4 h-4"><a href=""><img src="{{asset('assets/image/icon/whatsapp.svg')}}" alt="Facebook" class="w-full h-full"></a></span>
         </div>
     </div>
-    <div class="absolute w-screen bg-black top-0 z-10 opacity-65" style="height: 44rem"></div>
+    <div class="absolute w-screen bg-black top-0 z-10 opacity-65" style="height: 42rem"></div>
     <main class="flex justify-center">
-        <div class="container w-7/12 mb-14 mt-10">
+        <div class="container w-7/12 mb-14 mt-5">
             <h1 class="font-bold text-center text-slate-600 p-5 text-2xl">Welcome to Popmovies - Your Premier Free Streaming Destination</h1>
             <p class="mb-6 text-sm">In late 2016, we observed that many free movie streaming sites lacked an intuitive user interface (UI) and a seamless user experience (UX). To address this, our dedicated product development team launched Popmovies, aiming to deliver a superior streaming platform for all movie enthusiasts.</p>
 

4a89e23ebf7585d278e7eff0cc4ce6fbf8da9a10
rojay143-cj <rojaymerlin@gmail.com>
2024-07-30 10:33:02 +0800

    .

--- public/assets/animation/aniJS.js
+++ public/assets/animation/aniJS.js
@@ -1,40 +1,58 @@
 // Genre's dropdown ->
-$(function() {
-    const genre = $('.movies-genre');
-    
-    $('.drp-genre').click(function(e) {
+$(function () {
+    const genre = $(".movies-genre");
+    const sideBar = $("#side-bar");
+
+    $(".drp-genre").click(function (e) {
         e.stopPropagation();
         genre.slideToggle(300);
+        
+        if (sideBar.hasClass("left-0")) {
+            sideBar.removeClass("duration-500 left-0").addClass("-left-96");
+        }
     });
 
-    $(document).click(function(e) {
-        if (!$(e.target).closest('.movies-genre').length) {
+    $(document).click(function (e) {
+        if (!$(e.target).closest(".movies-genre").length) {
             genre.slideUp(0);
         }
     });
 
-    genre.click(function(e) {
+    genre.click(function (e) {
         e.stopPropagation();
     });
 });
 
-    // Side Bar Animation
-    $(function(){
-        var opensideBar;
-        
-        $('.open-side-bar').on('click', function() {
-            opensideBar = $('#side-bar').removeClass('-left-96');
-            $('#side-bar').addClass('duration-500 left-0');
-        });
-    
-        $('.close-side-bar').on('click', function() {
-            $('#side-bar').removeClass('duration-500 left-0');
-            $('#side-bar').addClass('-left-96');
-        });
-        $(document).on('click', function(event) {
-            if (!$(event.target).closest('#side-bar, .open-side-bar').length) {
-                $('#side-bar').removeClass('duration-500 left-0');
-                $('#side-bar').addClass('-left-96');
-            }
-        });
+// Side Bar Animation
+$(function () {
+    const sideBar = $("#side-bar");
+    const navBar = $("#nav-bar");
+    const opensidebar = $(".open-side-bar");
+    function handleNavBarVisibility() {
+        if ($(window).width() > 768) {
+            navBar.removeClass('hidden');
+        }else{
+            navBar.addClass('hidden');
+        }
+    }
+    $(".open-side-bar").on("click", function () {
+        sideBar.removeClass("-left-96").addClass("duration-500 left-0");
+        handleNavBarVisibility();
     });
+    $(".close-side-bar").on("click", function () {
+        sideBar.removeClass("duration-500 left-0").addClass("-left-96");
+        handleNavBarVisibility();
+    });
+    $(document).on("click", function (event) {
+        if (!$(event.target).closest("#side-bar, .open-side-bar").length) {
+            sideBar.removeClass("duration-500 left-0").addClass("-left-96");
+            handleNavBarVisibility();
+        }
+    });
+    $(window).on("resize", function () {
+        handleNavBarVisibility();
+    });
+    handleNavBarVisibility();
+
+    // Hide the nav-bar if I open the sidebar in bigger screen
+});
